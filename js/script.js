5ec899c4b9bb912b76ace23d3e83414e045927ba
Sebastian Faryna <sebastianfaryna@gmail.com>
2020-10-25 17:41:57 +0100

    Add clickable titles links in JS

--- js/script.js
+++ js/script.js
@@ -2,11 +2,6 @@
 
 {
 
-  // document.getElementById('test-button').addEventListener('click', function () {
-  //   const links = document.querySelectorAll('.titles a');
-  //   console.log('links:', links);
-  // });
-
   // Visual Studio Code gdy klikam Shift + Alt + F to dodaje spację po 'function' jak to zmienić? Oraz żeby domyślnie zawijało wiersze, bez klikania Alt + Z?
   const titleClickHandler = function (event) {
     event.preventDefault(); //zmienia domyślne ustawienia 'event' - w tym przypadku adres url strony NIE będzie się zmieniał wraz z klikaniem w artykuły, dlatego jest przed deklaracją zmiennej/stałej poniżej
@@ -40,16 +35,56 @@
 
     // find the correct article using the selector (value of 'href' attribute) //
     const targetArticle = document.querySelector(articleSelector);
-    console.log('FIND href targetArticle', targetArticle);
-
-    // add class 'active' to the correct article //
     targetArticle.classList.add('active');
+    console.log('FIND href targetArticle', targetArticle);
   }
 
-  const links = document.querySelectorAll('.titles a');
+  // Zapisanie ustawień skryptu w stałych. Prefiks opt- znaczy "options".
+  const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
 
-  for (let link of links) {
-    link.addEventListener('click', titleClickHandler);
+  function generateTitleLinks() {
+
+    // [DONE] remove contents of titleList
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+
+    let html = "";
+
+    // for each article
+    const articles = document.querySelectorAll(optArticleSelector);
+    for (let article of articles) {
+      console.log(article);
+
+      // get the article id and save it to the const
+      const articleId = article.getAttribute('id');
+      console.log('articleId:', articleId);
+
+      // find the title element & get the title from the title element
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+      console.log('articleTitle:', articleTitle);
+
+      // create HTML of the link and save it to the const
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      console.log(linkHTML);
+
+      // insert html link into titleList
+      html = html + linkHTML;
+
+    }
+
+    titleList.innerHTML = html;
+
+    // kliknięcia w linki
+    const links = document.querySelectorAll('.titles a');
+    console.log('links:', links);
+    for (let link of links) {
+      link.addEventListener('click', titleClickHandler);
+    }
+
   }
+
+  generateTitleLinks();
 
 }
